d92965e977dbbf84743e59504476980251fe6a81
lolddong <w2crosss@gmail.com>
2025-03-19 18:17:52 +0900

    gg

--- 02_NODEJS/01_NODE(심화)/01-goodbye-1.mjs
+++ 02_NODEJS/01_NODE(심화)/01-goodbye-1.mjs
@@ -1,3 +1,8 @@
-function goodbye() {
-  console.log('홍길동님, 안녕히 가세요.');
+const hi = () => {
+  return '홍길동님, 안녕하세요?';
+};
+export { hi };
+
+export default function goodbye() {
+  return '홍길동님, 안녕히 가세요.';
 }
