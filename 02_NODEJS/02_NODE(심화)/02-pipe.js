7e958612088ecfbf2ac5be81d62dd64471a706bd
Pearl <w2crosss@gmail.com>
2025-04-04 01:41:59 +0900

    update 0404

--- 02_NODEJS/02_NODE(심화)/02-pipe.js
+++ 02_NODEJS/02_NODE(심화)/02-pipe.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 
+// 스트림 사용
 const readStream = fs.createReadStream("./readMe.txt", "utf-8");
 const writeStream = fs.createWriteStream("./writeMe.txt");
 
+// 파이프 사용
 readStream.pipe(writeStream);
