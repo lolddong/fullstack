13f321a3d7f297accf68b68b594d78960709681f
lolddong <w2crosss@gmail.com>
2025-04-04 13:35:09 +0900

    update 0404

--- 02_NODEJS/02_NODE(기본)/03장_파일관리하기/02-list-1.js
+++ 02_NODEJS/02_NODE(기본)/03장_파일관리하기/02-list-1.js
@@ -1,5 +1,6 @@
 // fs (File System) 모듈 불러오기
 const fs = require('fs');
 
+// 현재 작업 디렉토리 파일 목록 출력 (동기 함수 사용)
 let files = fs.readdirSync('./');
-console.log(files);
+console.log(files); // [ '01-path.js', '02-list-1.js' ]
