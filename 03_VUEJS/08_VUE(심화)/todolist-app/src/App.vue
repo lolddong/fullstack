584c5f5c7b4936a5d5833d10d0fff9ca3f83334b
lolddong <w2crosss@gmail.com>
2025-04-14 18:02:12 +0900

    update0414

---
+++ 03_VUEJS/08_VUE(심화)/todolist-app/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import { reactive, onMounted } from 'vue'
+import TodoList from './components/TodoList.vue'
+import InputTodo from './components/InputTodo.vue'
+
+export default {
+  name: 'App',
+  components: { InputTodo, TodoList },
+  setup() {
+    const ts = new Date().getTime()
+    const state = reactive({ todoList: [] })
+
+    onMounted(() => {
+      state.todoList.push({ id: ts, todo: '자전거 타기', completed: false })
+    })
+  },
+}
+</script>
+
+<template>
+  <div id="app" class="container">
+    <div class="card card-body bg-light pt-5 pb-5">
+      <div class="title">:: Todolist App</div>
+    </div>
+    <div class="card card-default card-borderless">
+      <div class="card-body">
+        <InputTodo />
+        {{ ts }}
+        <TodoList :todoList="todoList" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ 03_VUEJS/08_VUE(심화)/todolist-app/src/components/TodoList.vue
@@ -0,0 +1,21 @@
+<script>
+import TodoListItem from './TodoListItem.vue'
+export default {
+  name: 'TodoList',
+  components: { TodoListItem },
+  props: {
+    todoList: { type: Array, required: true },
+  },
+  emits: ['delete-todo', 'toggle-completed'],
+}
+</script>
+
+<template>
+  <div class="row">
+    <div class="col">
+      <ul class="list-group">
+        <TodoListItem v-for="todoItem in todoList" :key="todoItem.id" :todoItem="todoItem" />
+      </ul>
+    </div>
+  </div>
+</template>
