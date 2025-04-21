b3eb9b940633edd208ca282ae6f309b30f32912e
lolddong <w2crosss@gmail.com>
2025-04-21 18:49:32 +0900

    update

---
+++ 03_VUEJS/07_VUE(기본)/v-model-test/src/components/InputName.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'InputName',
+  props: ['name'],
+}
+</script>
+
+<template>
+  <input type="text" :value="name" @input="$emit('update:name', $event.target.value)" />
+</template>
+
+<style></style>
