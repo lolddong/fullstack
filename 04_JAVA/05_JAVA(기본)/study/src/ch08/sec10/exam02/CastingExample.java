97abd72c33dfbd3174f2c0b2f91e05bbfe16dec0
lolddong <w2crosss@gmail.com>
2025-04-22 14:50:17 +0900

    05_JAVA(기본) done

--- 04_JAVA/05_JAVA(기본)/study/src/ch08/sec10/exam02/CastingExample.java
+++ 04_JAVA/05_JAVA(기본)/study/src/ch08/sec10/exam02/CastingExample.java
@@ -7,8 +7,7 @@ public class CastingExample {
 
         // 인터페이스를 토해 호출
         vehicle.run();
-        // vehicle.checkFare()
-        // Vehicle 인터페이스에는 checkFare() 메소드가 정의되지 않아 에러 발생
+        // vehicle.checkFare(); // Vehicle 인터페이스에는 checkFare() 메소드가 정의되지 않아 에러 발생
 
         // 강제 타입 변환 후 호출
         Bus bus = (Bus) vehicle;
