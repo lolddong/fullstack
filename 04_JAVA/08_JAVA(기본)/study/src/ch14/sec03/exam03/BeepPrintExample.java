4be309b368de1beb1863e28a90d4b30ccbd6bf55
lolddong <w2crosss@gmail.com>
2025-04-24 12:47:59 +0900

    08_JAVA(기본) done

---
+++ 04_JAVA/08_JAVA(기본)/study/src/ch14/sec03/exam03/BeepPrintExample.java
@@ -0,0 +1,26 @@
+package ch14.sec03.exam03;
+
+import java.awt.Toolkit;
+
+public class BeepPrintExample {
+    public static void main(String[] args) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                // 작업1 :0.5초 간격으로 비프음 출력
+                Toolkit toolkit = Toolkit.getDefaultToolkit();
+                for (int i = 0; i < 5; i++)  {
+                    toolkit.beep();
+                    try {Thread.sleep(500);} catch (Exception e) {}
+                }
+            }
+        };
+        thread.start();
+
+        // 작업2: 0.5초 간격으로 "띵" 문자열 출력 (main 스레드)
+        for (int i = 0; i < 5; i++) {
+            System.out.println("띵");
+            try {Thread.sleep(500);} catch (Exception e) {}
+        }
+    }
+}
