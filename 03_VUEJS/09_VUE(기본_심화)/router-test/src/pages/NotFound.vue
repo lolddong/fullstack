b4be3376cc985a5301db189becc05be645ab3cb2
Pearl <w2crosss@gmail.com>
2025-05-01 23:31:07 +0900

    09_VUE(기본_심화) done

---
+++ 03_VUEJS/09_VUE(기본_심화)/router-test/src/pages/NotFound.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card card-body">
+    <h2>404 Not Found</h2>
+    <!-- Options API -->
+    <!-- <p>존재하지 않는 요청 경로: {{ $route.fullPath }}</p> -->
+    <!-- Composition API -->
+    <p>존재하지 않는 요청 경로: {{ fullPath }}</p>
+  </div>
+</template>
+
+<script setup>
+// Options API
+// export default {
+//  name: 'NotFound',
+//  created() {
+//    console.log(this.$route.params)
+//  },
+// }
+
+// Composition API
+import { onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+const fullPath = route.fullPath
+
+onMounted(() => {
+  console.log(route.params)
+  console.log(router)
+})
+</script>
---
+++ 03_VUEJS/09_VUE(기본_심화)/router-test/src/router/index.js
@@ -0,0 +1,55 @@
+import { createRouter, createWebHistory, isNavigationFailure } from 'vue-router'
+
+import Home from '@/pages/Home.vue'
+import About from '@/pages/About.vue'
+import Members from '@/pages/Members.vue'
+import MemberInfo from '@/pages/MemberInfo.vue'
+import Videos from '@/pages/Videos.vue'
+import VideoPlayer from '@/pages/VideoPlayer.vue'
+import NotFound from '@/pages/NotFound.vue'
+
+// 내비게이션 가드
+const membersIdGuard = (to, from) => {
+  // members/:id 경로는 반드시 이전 경로가
+  // /members, /members/:id인 경우만 내비게이션 허용
+  if (from.name !== 'members' && from.name !== 'members/id') {
+    return false
+  }
+}
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', name: 'home', component: Home },
+    { path: '/about', name: 'about', component: About },
+    { path: '/members', name: 'members', component: Members },
+    {
+      path: '/members/:id',
+      name: 'memberInfo',
+      component: MemberInfo,
+      beforeEnter: membersIdGuard,
+    },
+    {
+      path: '/videos',
+      name: 'videos',
+      component: Videos,
+      children: [{ path: ':id', name: 'videos/id', component: VideoPlayer }],
+    },
+    { path: '/:paths(.*)*', name: 'NotFound', component: NotFound },
+  ],
+})
+
+router.beforeEach((to) => {
+  // to.query에 속성이 존재할 경우 제거된 경로로 재이동
+  if (to.query && Object.keys(to.query).length > 0) {
+    return { path: to.path, query: {}, params: to.params }
+  }
+})
+
+router.afterEach((to, from, failure) => {
+  if (isNavigationFailure(failure)) {
+    console.log('@@ 내비게이션 중단: ', failure)
+  }
+})
+
+export default router
