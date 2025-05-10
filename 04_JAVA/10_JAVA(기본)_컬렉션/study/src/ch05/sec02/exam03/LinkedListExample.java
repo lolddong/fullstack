d18bced60606dd5e8196909b07737e1351892344
Pearl <w2crosss@gmail.com>
2025-05-11 01:37:49 +0900

    10_JAVA 하는 중;

---
+++ 04_JAVA/10_JAVA(기본)_컬렉션/study/src/ch05/sec02/exam03/LinkedListExample.java
@@ -0,0 +1,32 @@
+package ch05.sec02.exam03;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        // ArrayList 컬렉션 갹체 생성
+        List<String> listArrayList = new ArrayList<>();
+        // LinkedList 컬렉션 갹체 생성
+        List<String> listLinkedList = new LinkedList<>();
+        // 시작 시간과 끝 시간을 저장할 변수 선언
+        long startTime;
+        long endTime;
+        // ArrayList 컬렉션에 저장하는 시간
+        startTime = System.nanoTime();
+        for(int i = 0; i < 10000; i++) {
+            listArrayList.add(0, String.valueOf(i));
+        }
+        endTime = System.nanoTime();
+        System.out.printf("%-20s %8d ns \n","ArrayList 걸린 시간: ", (endTime - startTime));
+        // LinkedList 컬렉션에 저장하는 시간long startTime;
+        startTime = System.nanoTime();
+        for(int i = 0; i < 10000; i++) {
+            listLinkedList.add(0, String.valueOf(i));
+        }
+        endTime = System.nanoTime();
+        System.out.printf("%-20s %8d ns \n","LinkedList 걸린 시간: ", (endTime - startTime));
+
+    }
+}
---
+++ 04_JAVA/10_JAVA(기본)_컬렉션/study/src/ch05/sec03/exam02/Member.java
@@ -0,0 +1,25 @@
+package ch05.sec03.exam02;
+
+public class Member {
+    public String name;
+    public int age;
+
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    // hashCode 재정의
+    @Override
+    public int hashCode() {
+        return name.hashCode() + age; // name과 age 값이 같으면 동일한 hashCode() 반환
+    }
+    // equals 재정의
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Member target) { // name과 age 값이 같으면 true 반환
+            return target.name.equals(name) && target.age == age;
+        } else {
+            return false;
+        }
+    }
+}
