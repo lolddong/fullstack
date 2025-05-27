1cc53ff52f2d6afbd3e9971688dcaeb4f2253001
Pearl <w2crosss@gmail.com>
2025-05-28 08:46:55 +0900

    04_MYSQL(기본) done

--- 04_JAVA/02_JAVA(기본)/study/src/ch05/sec04/NullPointerExceptionExample.java
+++ 04_JAVA/02_JAVA(기본)/study/src/ch05/sec04/NullPointerExceptionExample.java
@@ -4,7 +4,7 @@ public class NullPointerExceptionExample {
     public static void main(String[] args) {
         int[] intArray = null;
 //        intArray[0] = 10; // NullPointerException
-        String str = null;
+        String str = null;₩~
 //        System.out.println("총 문자 수: " + str.length()); // NullPointerException
     }
 }
