9f679991cdbbdc88165eb04f0e74e0ec08c73a66
Pearl <w2crosss@gmail.com>
2025-05-28 08:37:51 +0900

    update 0528

--- 04_JAVA/02_JAVA(기본)/study/src/ch05/sec03/ReferenceVariableCompareExample.java
+++ 04_JAVA/02_JAVA(기본)/study/src/ch05/sec03/ReferenceVariableCompareExample.java
@@ -2,15 +2,15 @@ package ch05.sec03;
 
 public class ReferenceVariableCompareExample {
     public static void main(String[] args) {
-        int[] arr1;
-        int[] arr2;
-        int[] arr3;
+        int[] arr1; // 배열 변수 arr1 선언
+        int[] arr2; // 배열 변수 arr2 선언
+        int[] arr3; // 배열 변수 arr3 선언
 
-        arr1 = new int[] {1, 2, 3};
-        arr2 = new int[] {1, 2, 3};
-        arr3 = arr2;
+        arr1 = new int[] {1, 2, 3}; // 배열 {1, 2, 3}을 생성하고 arr1 변수에 대립
+        arr2 = new int[] {1, 2, 3}; // 배열 {1, 2, 3}을 생성하고 arr2변수에 대립
+        arr3 = arr2; // 배열 변수가 arr2의 값을 배열 변수 arr3에 대입
 
-        System.out.println(arr1==arr2);
-        System.out.println(arr2==arr3);
+        System.out.println(arr1 == arr2);
+        System.out.println(arr2 == arr3);
     }
 }
